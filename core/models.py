86c7d6f6d3d95f066390c208ac2f695eb4342fa0
fabioangeli30 <fabioangeli26@gmail.com>
2022-05-27 20:18:12 +0200

    site cinema

---
+++ core/models.py
@@ -0,0 +1,49 @@
+import datetime
+from django.db import models
+# Create your models here.
+RUOLI= [
+    ('a', 'attore'),
+    ('r', 'regista'),
+    ('ar', 'attore/regista'),
+    ]
+
+class Persona(models.Model):
+    name=models.CharField(max_length=250,blank=True,null=True)
+    surname=models.CharField(max_length=250,blank=True,null=True)
+    role=models.CharField(max_length=50,
+                  choices=RUOLI,blank=True,null=True)
+
+
+
+class Film(models.Model):
+    cast= models.ManyToManyField(Persona,related_name='cast')
+    code_film = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, blank=True, null=True)
+    year = models.CharField(max_length=4,blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def get_year(self):
+        # format it to datetime object. You need to convert `year` to str if it is `IntergerField`. ex: str(self.year).
+        date = datetime.datetime.strptime('%Y', self.year)
+        return date
+
+class Proiezione(models.Model):
+    film= models.ForeignKey(Film, on_delete=models.CASCADE)
+    date= models.DateTimeField(blank=True,null=True)
+    number_spectators= models.IntegerField(blank=True,null=True)
+
+
+class Cinema(models.Model):
+    name=models.CharField(max_length=250,blank=True,null=True)
+    url=models.URLField(blank=True,null=True)
+    room_number=models.IntegerField(blank=True,null=True)
+
+class Sala(models.Model):
+    proiezione=models.ForeignKey(Proiezione, on_delete=models.CASCADE)
+    cinema=models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    is_3d=models.BooleanField(default=False,blank=True,null=True)
+
+
