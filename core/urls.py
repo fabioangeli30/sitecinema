b27516f0aeb395b5c2a5567e2826d404812c8544
fabioangeli30 <fabioangeli26@gmail.com>
2022-05-29 15:59:52 +0200

    fixed header and footer

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import TaskView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TaskView.as_view(), name='index'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,6 @@ from .models import Task
 from django.views.generic.list import ListView
 
 
-class ModelListView(ListView):
+class TaskView(ListView):
     model = Task
     template_name = "mainpage/mainpage.html"
