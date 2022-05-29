35e2b5a40e0cc9dbce162d3e50c18ab1cbc27e0f
fabioangeli30 <fabioangeli26@gmail.com>
2022-05-29 23:15:55 +0200

    add task create view form and task update view

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TaskView,TaskDetailView,TaskCreateView
+from .views import TaskView,TaskDetailView,TaskCreateView,TaskUpdateView
 
 urlpatterns = [
-    path('', TaskView.as_view(), name='index'),
+    path('', TaskView.as_view(), name='tasks'),
     path('task/<int:pk>/',TaskDetailView.as_view(), name='task'),
     path('create-task/',TaskCreateView.as_view(), name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdateView.as_view(), name='task-update'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView
 
 class TaskView(ListView):
     model = Task
@@ -19,3 +20,12 @@ class TaskDetailView(DetailView):
 class TaskCreateView(CreateView):
     model = Task
     fields = "__all__"
+    template_name = "mainpage/task_form.html"
+    success_url=reverse_lazy('tasks')
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    template_name = 'mainpage/task_update.html'
+    success_url=reverse_lazy('tasks')
