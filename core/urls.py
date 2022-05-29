90608e4d7104ce9ab53e970bc3e6242021b5499d
fabioangeli30 <fabioangeli26@gmail.com>
2022-05-29 16:25:18 +0200

    add form

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TaskView,TaskDetailView
+from .views import TaskView,TaskDetailView,TaskCreateView
 
 urlpatterns = [
     path('', TaskView.as_view(), name='index'),
-    path('task/<int:pk>/',TaskDetailView.as_view(), name='task')
+    path('task/<int:pk>/',TaskDetailView.as_view(), name='task'),
+    path('create-task/',TaskCreateView.as_view(), name='task-create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 
 class TaskView(ListView):
@@ -8,8 +9,13 @@ class TaskView(ListView):
     template_name = "mainpage/task_list.html"
     context_object_name = 'tasks'
 
+
 class TaskDetailView(DetailView):
     model = Task
     template_name = "mainpage/task_detail.html"
-    context_object_name= "task"
+    context_object_name = "task"
 
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = "__all__"
