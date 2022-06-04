77ebc57e86d7fdc369eda53dc0181d6ae3c5a1fb
fabioangeli30 <fabioangeli26@gmail.com>
2022-06-04 13:29:02 +0200

    add authmixin

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import TaskView,TaskDetailView,TaskCreateView,TaskUpdateView,TaskDeleteView,CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/',CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page='tasks'),name='logout'),
     path('', TaskView.as_view(), name='tasks'),
     path('task/<int:pk>/',TaskDetailView.as_view(), name='task'),
     path('create-task/',TaskCreateView.as_view(), name='task-create'),
--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import  LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class CustomLoginView(LoginView):
     template_name = 'login/custom_login.html'
@@ -14,33 +15,44 @@ class CustomLoginView(LoginView):
         return reverse_lazy("tasks")
 
 
-class TaskView(ListView):
+class TaskView(LoginRequiredMixin,ListView):
     model = Task
     template_name = "mainpage/task_list.html"
     context_object_name = 'tasks'
 
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['tasks']= context['tasks'].filter(user=self.request.user)
+        context['count']= context['tasks'].filter(complete=False)
+        return context
 
-class TaskDetailView(DetailView):
+
+class TaskDetailView(LoginRequiredMixin,DetailView):
     model = Task
     template_name = "mainpage/task_detail.html"
     context_object_name = "task"
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(LoginRequiredMixin,CreateView):
     model = Task
-    fields = "__all__"
+    fields = ['title', 'description', 'complete']
     template_name = "mainpage/task_form.html"
     success_url=reverse_lazy('tasks')
 
+    def form_valid(self,form):
+        form.instance.user=self.request.user
+        return super(TaskCreateView, self).form_valid(form)
 
-class TaskUpdateView(UpdateView):
+
+
+class TaskUpdateView(LoginRequiredMixin,UpdateView):
     model = Task
-    fields = "__all__"
+    fields = ['title', 'description', 'complete']
     template_name = 'mainpage/task_form.html'
     success_url=reverse_lazy('tasks')
 
 
-class  TaskDeleteView(DeleteView):
+class  TaskDeleteView(LoginRequiredMixin,DeleteView):
     model = Task
     context_object_name = 'task'
     template_name = "mainpage/task_delete.html"
